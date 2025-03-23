6dc2b12638bec986b001894e48af4343c6d479a8
CyTanvir <ahmedtanvir2004@gmail.com>
2025-03-23 00:43:04 -0400

    updated

--- src/Components/firebase/firebase.js
+++ src/Components/firebase/firebase.js
@@ -1,11 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCrP70yMAlrcT5Ssb8qUlQ7wLR0bks8jIA",
   authDomain: "parkfinder-a4469.firebaseapp.com",
@@ -18,8 +12,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export { auth, provider };
+export { app, auth, provider };
--- src/Components/main/main.jsx
+++ src/Components/main/main.jsx
@@ -2,39 +2,3 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './main.css';
 
-<html>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./main.css">
-    <link rel="stylesheet" href="media.css"> 
-<head>
-    
-    
-  
-</head>
-
-//body starts here
-<body>
-    <nav id="navigation">
-        <div class="Logo">Park Finder</div>
-        <ul>
-            <li><a href="mostvisited.html">Hot Spot</a></li>
-            <li><a href="hotspot.html">Most Visited</a></li>
-            <li><a href="contactus.html">Contact Us</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="account.html">Your Account</a></li>
-        </ul>
-    </nav>
-        
-    <div id="mid">
-        <div class="mid-h1"> Park Finder </div>
-        <div class="mid-a1"> Thank you for vising park finder. we priortize our customer and make sure they get the best 
-            expriance. Through this park finder you can easily view diffrent parks and see thier reviews
-        you can also review these website. </div>
-
-        </div>
-
-</body>
-
-</html>
-
-<script src="script.js"></script>
